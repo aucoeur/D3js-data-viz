c37ca30ea9fa5f77f14d9ec620c00b73e6da65c7
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-03-01 14:30:51 -0800

    tweaked top10 scale

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log('boop')
 
 const width = 500;
-const height = 600;
+const height = 200;
 const padding = 2;
 const barHeight = 10;
 
+// TODO: Add axis label
 function showTopTen() {
 
   const xScale = d3
@@ -13,35 +14,33 @@ function showTopTen() {
     .range([width, 0]) // The range is the output value
     .domain([
       d3.min(data, (d) => d.score),
-      // d3.max(data, (d) => d.score)
-      10
+      d3.max(data, (d) => d.score)
     ])
 
-  const top10 = d3.select('.main')
+  const div = d3.select('.main')
     .append("div")
-    .attr("id", "top10")
+      .attr("id", "top10")
 
-  const t10Header = top10
-    .append("h4")
+  // Header
+  div.append("h4")
     .text("Top Ten Ranked Countries")
-  const topTenSVG = top10.append("svg")
+  const svg = div.append("svg")
     .attr("id", "topTen")
     .attr("viewBox", [0, 0, width, height])
 
-  const g = topTenSVG.selectAll("g")
+  const g = svg.selectAll("g")
     .data(data.slice(0, 10))
     .join("g")
-    .attr("id", (d) => d.country)
+      .attr("id", (d) => d.country)
 
-  const bar = g.append("rect")
-    // .selectAll("rect")
-    // .join("rect")
+  // Bar
+  g.append("rect")
     .attr("fill", "rgb(201, 144, 0)")
     .attr("x", 0)
     .attr("y", (_, i) => (i * (barHeight + padding)))
     // .attr("width", (d, i) => d.score * 50)
     .attr("width", (d) => {
-      return width - xScale(d.score)
+      return width*0.8 - xScale(d.score)*1.25
     })
     .attr("height", barHeight)
 
@@ -52,6 +51,13 @@ function showTopTen() {
     .attr("dy", "0.35em")
     .attr("fill", 'white')
     .attr('font-size', `${barHeight-2}px`)
+
+}
+
+function showParallelPlot() {
+  const div = d3.select('.main')
+    .append("div")
+      .attr("id", "parallel")
 
 }
 
