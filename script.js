235f486cda1c3c9abade9ae03ba8498117d50c99
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-03-01 18:14:09 -0800

    add x axis

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-console.log('boop')
+// Working with the top ten only
+let data10 = data.slice(0, 10)
+const numItems = 10;
 
-const width = 500;
-const height = 200;
+const margin = ({top: 20, right: 100, bottom: 30, left: 40})
 const padding = 2;
 const barHeight = 10;
+const width = 500;
+const height = numItems * (barHeight + padding)+ margin.bottom;
+
+const x = d3.scaleLinear()
+  .domain([0, 10])
+  .range([margin.left, width - margin.right])
+
+
+const xAxis = g => g
+  .attr("transform", `translate(0, ${height-margin.bottom})`)
+  .call(d3.axisBottom(x))
+
+// const y = d3.scaleLinear()
+//   .domain([0, 1])
+//   .range([height - margin.bottom, margin.top])
+
+// const yAxis = g => g
+//   .attr("transform", `translate(${margin.left},0)`)
+//   .call(d3.axisLeft(y))
 
 // TODO: Add axis label
 function showTopTen() {
 
   const xScale = d3
     .scaleLinear()
-    // Use d3.extent() to find the min and max
-    .range([width, 0]) // The range is the output value
-    .domain([
-      d3.min(data, (d) => d.score),
-      d3.max(data, (d) => d.score)
-    ])
+    .range([margin.left, width-margin.right]) // The range is the output value
+    .domain(
+      [0,numItems+2] // accounts for starting the axis at 0
+      // d3.extent(data10, (d) => d.score)
+    )
+
+  const xAxisLines = d3.axisBottom(x)
+    .tickSize(-(height - margin.top))
+    .tickFormat('')
+    .ticks()
 
   const div = d3.select('.main')
     .append("div")
@@ -24,14 +48,35 @@ function showTopTen() {
   // Header
   div.append("h4")
     .text("Top Ten Ranked Countries")
+
   const svg = div.append("svg")
     .attr("id", "topTen")
     .attr("viewBox", [0, 0, width, height])
 
-  const g = svg.selectAll("g")
-    .data(data.slice(0, 10))
+  // Axis & Axis Lines
+  d3.select("#topTen")
+    .append("g")
+      .attr("class", "axis")
+      .attr("stroke-width", ".5")
+      .attr("stroke-dasharray", "1,1")
+      .attr("transform", `translate(0, ${height-margin.bottom})`)
+      .call(xAxisLines)
+
+  d3.select("#topTen")
+    .append("g")
+      .call(xAxis)
+
+  // Bar Groups
+  d3.select("#topTen")
+    .append("g")
+      .attr("id", "bar")
+
+  const g = svg.select("#bar")
+    .selectAll("g")
+    .data(data10)
     .join("g")
       .attr("id", (d) => d.country)
+      .attr("transform", `translate(${margin.left}, 0)`)
 
   // Bar
   g.append("rect")
@@ -39,15 +84,17 @@ function showTopTen() {
     .attr("x", 0)
     .attr("y", (_, i) => (i * (barHeight + padding)))
     // .attr("width", (d, i) => d.score * 50)
-    .attr("width", (d) => {
-      return width*0.8 - xScale(d.score)*1.25
+    .attr("width", (d, i) => {
+      return xScale(d.score)
     })
     .attr("height", barHeight)
 
   g.append("text")
     .text((d) => `${d.rank} - ${d.country} - ${d.score}`)
     .attr("x", 5)
-    .attr("y", (_, i) => (i * (barHeight + padding) + barHeight / 2))
+    .attr("y", (_, i) => {
+        return (i * (barHeight + padding) + barHeight / 2)
+      })
     .attr("dy", "0.35em")
     .attr("fill", 'white')
     .attr('font-size', `${barHeight-2}px`)
@@ -59,6 +106,12 @@ function showParallelPlot() {
     .append("div")
       .attr("id", "parallel")
 
+  const svg = div.append("svg")
+    .attr("id", "parallelPlot")
+    .attr("viewBox", [0, 0, width, height])
+
+  const g = svg.selectAll("g")
+    .data(data10)
 }
 
 showTopTen()
